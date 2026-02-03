62e4ec2fc8df29d7dc379b3d521fe24b99dffe3b
aufafaf <mfaufalahatx@gmail.com>
2026-02-03 17:08:22 +0700

    add fitur

--- components/MediaPlayer.tsx
+++ components/MediaPlayer.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { MediaFile } from '@/types';
-import { useState } from 'react';
+import { MediaFile } from "@/types";
+import { useState } from "react";
 
 interface MediaPlayerProps {
   media: MediaFile & { webViewLink?: string };
@@ -10,21 +10,34 @@ interface MediaPlayerProps {
 export default function MediaPlayer({ media }: MediaPlayerProps) {
   const [imageError, setImageError] = useState(false);
 
+  // if (media.isVideo) {
+  //   return (
+  //     <div className="media-player">
+  //       <video
+  //         controls
+  //         className="video-player"
+  //         // poster={media.thumbnailLink}
+  //         poster={media.thumbnailLink || undefined}
+
+  //       >
+  //         {/* <source src={media.webContentLink} type={media.mimeType} /> */}
+  //         <source src={media.webContentLink ?? undefined} type={media.mimeType} />
+
+  //         Browser Anda tidak mendukung video player.
+  //       </video>
+  //     </div>
+  //   );
+  // }
+
   if (media.isVideo) {
     return (
       <div className="media-player">
-        <video
-          controls
-          className="video-player"
-          // poster={media.thumbnailLink}
-          poster={media.thumbnailLink || undefined}
-
-        >
-          {/* <source src={media.webContentLink} type={media.mimeType} /> */}
-          <source src={media.webContentLink ?? undefined} type={media.mimeType} />
-
-          Browser Anda tidak mendukung video player.
-        </video>
+        <iframe
+          src={`https://drive.google.com/file/d/${media.id}/preview`}
+          className="video-embed"
+          allow="autoplay"
+          allowFullScreen
+        />
       </div>
     );
   }
@@ -36,7 +49,6 @@ export default function MediaPlayer({ media }: MediaPlayerProps) {
           <img
             // src={media.webContentLink || media.thumbnailLink}
             src={media.webContentLink ?? media.thumbnailLink ?? undefined}
-
             alt={media.name}
             className="image-viewer"
             onError={() => setImageError(true)}
